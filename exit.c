c3e44a5b96011bd31b94cb95a923f06ffbedbc1b
kenny0595 <kennyyoung954@gmial.com>
2023-10-18 07:19:48 -0700

    the exit update

--- exit.c
+++ exit.c
@@ -3,9 +3,10 @@
 /**
   * execute_exit - exit the shell with the specified status
   *
-  * @args: an array of strings representing the command and its arguments
+  * @args: the command and its arguments are represented as an array of strings.
   *
   * Return: returns nothing
+  * this code is written by KENNY AND MBULA
   */
 
 void execute_exit(char **args)
